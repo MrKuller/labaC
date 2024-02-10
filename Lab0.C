0b2e5c62d0be5c9c638916db5a3da7d7d6e78fef
MrKuller <96410316+MrKuller@users.noreply.github.com>
2024-02-10 13:12:49 +1000

    Update Lab0.C

--- Lab0.C
+++ Lab0.C
@@ -1,6 +1,12 @@
+#include <stdio.h>
+#include <math.h>
 int main()
 {
-    printf("Hello World");
+    double a=3, b=4, c=-2,v,d,d2;
+    v = (b*b) - (4*a*c);
+    d = (-b + sqrt(v))/(2*a);
+    d2 = (-b - sqrt(v))/(2*a);
+    printf("i=%1f \n", v);
+    printf("x1=%1f,x2=%1f", d,d2);
 
     return 0;
-}
