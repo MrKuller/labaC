f67abd80e3469b6f7c735ffc6490a3bb13f5664b
MrKuller <96410316+MrKuller@users.noreply.github.com>
2024-02-10 13:21:04 +1000

    Update Lab0.C

--- Lab0.C
+++ Lab0.C
@@ -2,12 +2,25 @@
 #include <math.h>
 int main()
 {
-    double a=3, b=4, c=-2,v,d,d2;
+    double a=4, b=4, c=1,v,d,d2;
+    printf("Введите числа");
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
     v = (b*b) - (4*a*c);
-    d = (-b + sqrt(v))/(2*a);
-    d2 = (-b - sqrt(v))/(2*a);
-    printf("i=%1f \n", v);
-    printf("x1=%1f,x2=%1f", d,d2);
+    if(v>0){
+        d = (-b + sqrt(v))/(2*a);
+        d2 = (-b - sqrt(v))/(2*a);
+        printf("i=%1f \n", v);
+        printf("x1=%1f,x2=%1f", d,d2);
+    }
+    if(v==0){
+        d = (-b + sqrt(v))/(2*a);
+        printf("x1=%1f",d);
+    }
+    if(v<0){
+        printf("Корней нет");
+    }
 
     return 0;
 }
